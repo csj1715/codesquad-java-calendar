d5ed0345e43e2dd1c57789294dd0a7ed3c481159
SungJin <SungJin@DESKTOP-RPMKN6F>
2019-08-30 11:38:13 +0900

    Add repeat function

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+	
+	
 	public int getMaxDaysofMonth(int month) {
 		return MAX_DAYS[month -1];
 	}
@@ -23,10 +24,16 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		int month;
-		System.out.println("달을 입력하세요.");
-		month = sc.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysofMonth(month));
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat = sc.nextInt();
+		
+		for(int i=0; i< repeat; i++) {
+			System.out.println("달을 입력하세요.");
+			int month = sc.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysofMonth(month));			
+		}
+		System.out.println("Bye~~");
+			
 		sc.close();
 	}
 }
